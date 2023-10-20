67136724b199b7720105ae5ddd85928d79412089
Bogdan Filip <bogdan.filip@tecknoworks.com>
2023-10-20 14:30:05 +0300

    Made communication between backend and frontend (+CORS) CRUD on backend (example CRUD) CRUD request on frontend (working and made with axios)

---
+++ event-planner-frontend/src/services/userServices.ts
@@ -0,0 +1,21 @@
+import CommonService from "./commonServices";
+
+export default class UserServices extends CommonService {
+
+    getUsers(params?: object) {
+        return this.get("/getUsers", params);
+    }
+
+    createUser(data: object) {
+        return this.post("/createUser", data);
+    }
+
+    updateUser(id: number, data: object) {
+        return this.put(`/updateUser/${id}`, data);
+    }
+
+    deleteUser(id: number) {
+        return this.delete(`/deleteUser/${id}`);
+    }
+}
+
