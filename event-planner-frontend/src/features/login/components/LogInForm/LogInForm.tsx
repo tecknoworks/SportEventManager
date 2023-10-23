9ebe2c49f108e583a1b317481ecfadf927a6cc0b
unknown <miha_ionel@yahoo.com>
2023-10-24 00:21:14 +0300

    Form with validation

--- event-planner-frontend/src/features/login/components/LogInForm/LogInForm.tsx
+++ event-planner-frontend/src/features/login/components/LogInForm/LogInForm.tsx
@@ -1,84 +1,115 @@
-import React from 'react'
-import "common/styles/form.scss"
+import React from 'react';
+import 'common/styles/form.scss';
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Stack,
-    Text
-} from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Text,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const LogInForm = () => {
-    const navigate=useNavigate()
+  const navigate = useNavigate();
 
-    const [showPw, setShowPw] = React.useState<boolean>(false)
-    const [isDisabled, setIsDisabled] = React.useState<boolean>(true)
+  const [showPw, setShowPw] = React.useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = React.useState<boolean>(true);
 
-    const [userNameOrEmail, setUserNameOrEmail] = React.useState<string>("")
-    const [password, setPassword] = React.useState<string>("");
+  const [userNameOrEmail, setUserNameOrEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
 
+  type FormErrorMessage = { userNameOrEmail: string; password: string };
+  const [errors, setErrors] = React.useState<FormErrorMessage>({
+    userNameOrEmail: '',
+    password: '',
+  });
 
-    React.useEffect(() => {
-        if (userNameOrEmail !== "" && password !== "") {
-            setIsDisabled(false);
-        } else {
-            setIsDisabled(true);
-        }
-    }, [userNameOrEmail, password]);
+  React.useEffect(() => {
+    let tempErrors = { userNameOrEmail: '', password: '' };
 
-    const handleClickShowPw = () => setShowPw(!showPw)
+    const regex =
+      /^(?:[A-Z\d][A-Z\d_-]{5,30}|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})$/i;
 
+    if (!regex.test(userNameOrEmail)) {
+      tempErrors.userNameOrEmail = 'Enter a valid username or email';
+    }
 
-    return (
-        <Box
-            className="form-wrapper"
-            display="flex"
-            width="500px"
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-        >
-            <Text color="gray.500" as="b" fontSize="3xl">
-                Log In
-            </Text>
-            <form className="form-container">
-                <Stack spacing={5}>
-                    <FormControl isRequired>
-                        <FormLabel>User name / Email</FormLabel>
-                        <Input type="text" placeholder="User name / Email" onChange={(e) => setUserNameOrEmail(e.target.value)} />
-                    </FormControl>
-                    <FormControl isRequired>
-                        <FormLabel>Password</FormLabel>
-                        <InputGroup size="md">
-                            <Input
-                                pr="75px"
-                                type={showPw ? "text" : "password"}
-                                placeholder="Enter password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <InputRightElement width="75px">
-                                <Button h="28px" size="sm" onClick={handleClickShowPw} >
-                                    {showPw ? "Hide" : "Show"}
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
-                    <Button color="#610C9F" size="md" variant="solid" isDisabled={isDisabled}>
-                        Log In
-                    </Button>
-                    <Button variant="text" size="sm" onClick={()=>{navigate("/")}}>
-                        Forgot password?
-                    </Button>
-                </Stack>
+    if (!password) {
+      tempErrors.password = 'Required';
+    }
+    setErrors(tempErrors);
 
-            </form>
-        </Box>
-    )
-}
+    setIsDisabled(Object.values(tempErrors).some((error) => error !== ''));
+  }, [userNameOrEmail, password]);
 
-export default LogInForm
+  const handleClickShowPw = () => setShowPw(!showPw);
+
+  return (
+    <Box
+      className="form-wrapper"
+      display="flex"
+      width="500px"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+    >
+      <Text color="gray.500" as="b" fontSize="3xl">
+        Log In
+      </Text>
+      <form className="form-container">
+        <Stack spacing={5}>
+          <FormControl isRequired isInvalid={!!errors.userNameOrEmail}>
+            <FormLabel>User name / Email</FormLabel>
+            <Input
+              type="text"
+              placeholder="User name / Email"
+              onChange={(e) => setUserNameOrEmail(e.target.value)}
+            />
+            <FormErrorMessage>{errors.userNameOrEmail}</FormErrorMessage>
+          </FormControl>
+          <FormControl isRequired isInvalid={!!errors.password}>
+            <FormLabel>Password</FormLabel>
+            <InputGroup size="md">
+              <Input
+                pr="75px"
+                type={showPw ? 'text' : 'password'}
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width="75px">
+                <Button h="28px" size="sm" onClick={handleClickShowPw}>
+                  {showPw ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormErrorMessage>{errors.password}</FormErrorMessage>
+          </FormControl>
+          <Button
+            color="#610C9F"
+            size="md"
+            variant="solid"
+            isDisabled={isDisabled}
+          >
+            Log In
+          </Button>
+          <Button
+            variant="text"
+            size="sm"
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Forgot password?
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
+
+export default LogInForm;
