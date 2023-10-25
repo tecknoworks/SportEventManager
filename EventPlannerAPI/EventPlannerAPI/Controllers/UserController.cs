9f1729b1948f0448e15760013d680d048dc7a3d3
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-10-25 12:31:56 +0300

    rename stuff & delete unused deps

--- EventPlannerAPI/EventPlannerAPI/Controllers/UserController.cs
+++ EventPlannerAPI/EventPlannerAPI/Controllers/UserController.cs
@@ -9,18 +9,18 @@ namespace EventPlannerAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IUserService _userLogicService;
+        private readonly IUserService _userService;
 
         public UserController(IUserService userLogicService)
         {
-            _userLogicService = userLogicService;
+            _userService = userLogicService;
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
         [HttpPost("ResetPassword")]
         public async Task<IActionResult> ResetPassword([FromBody] ForgotPasswordDto forgotPasswordDto)
         {
-            var errorMessage = await _userLogicService.SendPasswordResetLinkAsync(forgotPasswordDto);
+            var errorMessage = await _userService.SendPasswordResetLinkAsync(forgotPasswordDto);
             if (!errorMessage.IsNullOrEmpty()) return BadRequest(errorMessage);
             return Ok("If there's an account associated with this email address, we've sent instructions for resetting the password.");
         }
@@ -30,7 +30,7 @@ namespace EventPlannerAPI.Controllers
         [HttpPost("SetNewPassword")]
         public async Task<IActionResult> SetNewPassword([FromBody] SetNewPasswordDto setNewPasswordDto)
         {
-            var result = await _userLogicService.SetNewPasswordAsync(setNewPasswordDto);
+            var result = await _userService.SetNewPasswordAsync(setNewPasswordDto);
             if (!result.Succeeded) return BadRequest(result.Errors);
             return Ok("Password reset successfully.");   
         }
