2d99edce13c80698a675d663cbed64597a344801
Bogdan Filip <bogdan.filip@tecknoworks.com>
2023-10-26 15:36:11 +0300

    Removed console logs and files that are not used

--- event-planner-frontend/src/App.tsx
+++ event-planner-frontend/src/App.tsx
@@ -3,47 +3,6 @@ import { useEffect } from 'react';
 import CommonService from 'services/commonServices';
 
 function App() {
-  const userService: UserServices = new UserServices();
-
-  useEffect(() => {
-    userService
-      .getUsers()
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-      });
-
-    let data: object = { name: 'Mirel' };
-    userService
-      .createUser(data)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-      });
-
-    userService
-      .updateUser(10, data)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err.response.data);
-      });
-
-    userService
-      .deleteUser(10)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log('not ok', err.response.data);
-      });
-  }, []);
-
   return <div>Event Planner</div>;
 }
 
