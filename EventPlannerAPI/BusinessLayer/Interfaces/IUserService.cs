bd1e1cd9080f9586425ad081bbb89dec7b7ac5e8
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-10-30 13:51:32 +0200

    change launch settings back

--- EventPlannerAPI/BusinessLayer/Interfaces/IUserService.cs
+++ EventPlannerAPI/BusinessLayer/Interfaces/IUserService.cs
@@ -1,12 +1,11 @@
 ﻿using BusinessLayer.DTOs;
-using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace BusinessLayer.Interfaces
 {
     public interface IUserService
     {
-		Task<IdentityResult> CreateUserAsync(RegisterUserDto user);
+        Task<IdentityResult> CreateUserAsync(RegisterUserDto user);
         Task<string> SendPasswordResetLinkAsync(ForgotPasswordDto forgotPasswordDto);
         Task<IdentityResult> SetNewPasswordAsync(SetNewPasswordDto setNewPasswordDto);
         Task<bool> LogInAsync(LogInUserDto eventPlannerUser);
