2fe29553b0747d3efbf761c5582f8c084d29445f
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-11-06 21:54:37 +0200

    add link to create event page to the navbar

--- event-planner-frontend/src/common/components/NavigationMenu/NavigationMenu.tsx
+++ event-planner-frontend/src/common/components/NavigationMenu/NavigationMenu.tsx
@@ -49,6 +49,12 @@ const NavigationMenu = () => {
       availableForUser: false,
       linkTo: '/adminusermanagement',
     },
+    {
+      key: 3,
+      title: 'Create event',
+      availableForUser: true,
+      linkTo: '/create-event',
+    },
   ];
 
   const token = useSelector(selectToken);
