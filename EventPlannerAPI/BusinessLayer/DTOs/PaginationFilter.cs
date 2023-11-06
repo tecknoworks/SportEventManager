332d494ff195b879d15da4a053638ae28e967d19
Bogdan Filip <bogdan.filip@tecknoworks.com>
2023-11-06 12:51:56 +0200

    Commited DTO for filters

---
+++ EventPlannerAPI/BusinessLayer/DTOs/PaginationFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTOs
+{
+    public class PaginationFilter
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public PaginationFilter()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 5;
+        }
+        public PaginationFilter(int pageNumber, int pageSize)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+        }
+    }
+}
