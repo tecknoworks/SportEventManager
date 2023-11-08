ca335c736508563211a88efd5426f8ab258c05e2
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-11-08 10:51:51 +0200

    add phone number constraint

--- EventPlannerAPI/BusinessLayer/DTOs/RegisterWithRoleDto.cs
+++ EventPlannerAPI/BusinessLayer/DTOs/RegisterWithRoleDto.cs
@@ -10,11 +10,17 @@ namespace BusinessLayer.DTOs
 {
     public class RegisterWithRoleDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [MinLength(10, ErrorMessage = "Phone number must be at least 10 digits long.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Phone number must be only digits.")]
         public string PhoneNumber { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public RoleType Role { get; set; }
     }
 }
