bd2e36bb1d547fc859457fa3e187b084c9e722d3
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-11-08 17:36:10 +0200

    create db structure

--- EventPlannerAPI/DataAccessLayer/Models/EventPlannerUser.cs
+++ EventPlannerAPI/DataAccessLayer/Models/EventPlannerUser.cs
@@ -7,5 +7,7 @@ namespace DataAccessLayer.Models
         public UserProfileDetails Profile { get; set; }
         public ICollection<Event> Events { get; set; }
         public ICollection<Participant> Participants { get; set; }
+        public ICollection<Review> GivenReviews { get; set; }
+        public ICollection<Review> RecievedReviews { get; set; }
     }
 }
