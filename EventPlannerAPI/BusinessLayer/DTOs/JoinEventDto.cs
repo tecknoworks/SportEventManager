a4fbbb9049c6c948775c8a8f51bdcded4990b664
Fabian Bob Mihai <mihai.fabian-bob@tecknoworks.com>
2023-11-14 17:46:26 +0200

    Fix bug BE(join event without position)

--- EventPlannerAPI/BusinessLayer/DTOs/JoinEventDto.cs
+++ EventPlannerAPI/BusinessLayer/DTOs/JoinEventDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,11 @@ namespace BusinessLayer.DTOs
 {
     public class JoinEventDto
     {
-        [Required] 
+        [Required]
         public string UserId { get; set; }
         [Required]
         public Guid EventId { get; set; }
+       
         public Guid? EventPositionId { get; set; }
     }
 }
