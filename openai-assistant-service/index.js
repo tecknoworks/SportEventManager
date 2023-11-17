2f8ff619ba3e0224042e3c6376321e65657007f9
Marco Munteanu <marcomunteanu02@gmail.com>
2023-11-17 18:20:21 +0200

    expose endpoints for create assistant and thread

--- openai-assistant-service/index.js
+++ openai-assistant-service/index.js
@@ -1,11 +1,47 @@
 const express = require("express");
+const config = require("dotenv").config();
 const app = express();
-require("dotenv").config();
+const cors = require("cors");
+const {
+  createAssistant,
+  getAssistantParamsTemplate,
+} = require("./src/createAssistant");
+const { createThread, getThreadParamsTemplate } = require("./src/createThread");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/test", async (req, res) => {
   res.json({ ana: "are mere" });
+});
+
+app.post("/assistants", async (req, res) => {
+  try {
+    let params = getAssistantParamsTemplate();
+
+    Object.assign(params, req.body);
+
+    const assistantDetails = await createAssistant(params);
+
+    res.json({ assistant: assistantDetails });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/assistants/:assistantId/threads", async (req, res) => {
+  try {
+    const { assistantId } = req.params;
+    let params = getThreadParamsTemplate();
+    params.inputAssistantId = assistantId;
+    params.userId = req.body.userId;
+
+    const threadDetails = await createThread(params);
+
+    res.json({ thread: threadDetails });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
