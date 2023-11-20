97517bc1da9593d1c08cfffcf1d1b08e8573ca80
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-11-20 13:30:49 +0200

    create message action

--- openai-assistant-service/index.js
+++ openai-assistant-service/index.js
@@ -2,17 +2,15 @@ const express = require("express");
 const config = require("dotenv").config();
 const app = express();
 const cors = require("cors");
-const {
-  createAssistant,
-  getAssistantParamsTemplate,
-} = require("./src/createAssistant");
+const { createAssistant, getAssistantParamsTemplate } = require("./src/createAssistant");
 const { createThread, getThreadParamsTemplate } = require("./src/createThread");
+const { createMessage, getMessageParamsTemplate } = require("./src/createMessage");
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req, res) => {
-  res.json({ ana: "are mere" });
+app.get("/test", async (_, res) => {
+  res.json({ status: "up" });
 });
 
 app.post("/assistants", async (req, res) => {
@@ -39,6 +37,23 @@ app.post("/assistants/:assistantId/threads", async (req, res) => {
     const threadDetails = await createThread(params);
 
     res.json({ thread: threadDetails });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/assistants/:assistantId/threads/:threadId", async (req, res) => {
+  try {
+    const { assistantId, threadId } = req.params;
+    let params = getMessageParamsTemplate();
+    params.assistantId = assistantId;
+    params.threadId = threadId;
+    params.userQuestion = req.body.userQuestion;
+    params.userId = req.body.userId;
+
+    const messages = await createMessage(params);
+
+    res.json({ messages: messages });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- openai-assistant-service/src/createMessage.js
+++ openai-assistant-service/src/createMessage.js
@@ -1,6 +1,15 @@
 const OpenAI = require("openai");
 
-export async function run(params) {
+function getMessageParamsTemplate() {
+  return {
+    threadId: "",
+    userQuestion: "",
+    userId: "",
+    assistantId: "",
+  };
+}
+
+async function createMessage(params) {
   const secretKey = process.env.OPENAI_API_KEY;
   const openai = new OpenAI({
     apiKey: secretKey,
@@ -9,37 +18,18 @@ export async function run(params) {
   const userQuestion = params.userQuestion;
   const inputThreadId = params.threadId;
   const inputUserId = params.userId;
+  const inputAssistantId = params.assistantId;
 
-  const threadRecords = await api.thread.findMany({
-    select: {
-      id: true,
-      description: true,
-      openAiId: true,
-      assistant: {
-        id: true,
-        openAiId: true,
-      },
-    },
-    filter: {
-      id: { equals: inputThreadId },
-    },
-  });
-
-  // Store the OpenAI IDs for the thread and assistant
-  const threadId = threadRecords[0].openAiId;
-  const assistantOpenAiId = threadRecords[0].assistant.openAiId;
-
-  // Create the user message in the OpenAI API
-  await openai.beta.threads.messages.create(threadId, {
+  await openai.beta.threads.messages.create(inputThreadId, {
     role: "user",
     content: userQuestion,
   });
 
-  const run = await openai.beta.threads.runs.create(threadId, {
-    assistant_id: assistantOpenAiId,
+  const run = await openai.beta.threads.runs.create(inputThreadId, {
+    assistant_id: inputAssistantId,
   });
 
-  let runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
+  let runStatus = await openai.beta.threads.runs.retrieve(inputThreadId, run.id);
 
   let attempts = 0;
   const maxAttempts = 20;
@@ -47,72 +37,21 @@ export async function run(params) {
 
   while (runStatus.status !== "completed" && attempts < maxAttempts) {
     await new Promise((resolve) => setTimeout(resolve, timoutWaitTimeMs));
-    runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
+    runStatus = await openai.beta.threads.runs.retrieve(inputThreadId, run.id);
     attempts++;
   }
 
-  // Get the last assistant and user messages from the messages array that contain all past messages
-  const messages = await openai.beta.threads.messages.list(threadId);
+  const messages = await openai.beta.threads.messages.list(inputThreadId);
   console.info(messages);
   const lastAssistantMessageForRun = messages.data
-    .filter(
-      (message) => message.run_id === run.id && message.role === "assistant"
-    )
+    .filter((message) => message.run_id === run.id && message.role === "assistant")
     .pop();
 
-  const userMessages = messages.body.data.filter(
-    (message) => message.role === "user"
-  );
+  const userMessages = messages.body.data.filter((message) => message.role === "user");
   const lastUserMessage = userMessages[0];
 
-  try {
-    const messageRecord = await api.message.create({
-      assistantMessage: lastAssistantMessageForRun,
-      thread: {
-        _link: threadRecords[0].id,
-      },
-      user: {
-        _link: inputUserId,
-      },
-      userMessage: lastUserMessage,
-    });
-
-    if (threadRecords[0].description === null) {
-      try {
-        const completion = await openai.chat.completions.create({
-          messages: [
-            {
-              role: "system",
-              content: `Provide a description for the question "${userQuestion}" using no more than 6 words. Don't use quotation marks or other characters when not needed.`,
-            },
-          ],
-          model: "gpt-3.5-turbo",
-        });
-
-        const threadRecord = await api.thread.update(inputThreadId, {
-          description: completion.choices[0].message.content,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    // This is what this function returns to the frontend, and is not used there yet
-    scope.result = {
-      success: true,
-    };
-  } catch (error) {
-    console.log(error);
-
-    // This is what this function returns to the frontend, and is not used there yet
-    scope.result = {
-      success: false,
-    };
-  }
+  //TODO: store message record to db
+  return messages;
 }
 
-export const params = {
-  threadId: { type: "string" },
-  userQuestion: { type: "string" },
-  userId: { type: "string" },
-};
+module.exports = { createMessage, getMessageParamsTemplate };
