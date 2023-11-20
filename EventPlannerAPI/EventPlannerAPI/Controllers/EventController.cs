b5cac91099745def113d903420290a830e6eefa9
Fabian Bob Mihai <mihai.fabian-bob@tecknoworks.com>
2023-11-20 16:34:25 +0200

    Resolve comment

--- EventPlannerAPI/EventPlannerAPI/Controllers/EventController.cs
+++ EventPlannerAPI/EventPlannerAPI/Controllers/EventController.cs
@@ -13,7 +13,6 @@ namespace EventPlannerAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class EventController : ControllerBase
     {
         private readonly IEventService _eventService;
